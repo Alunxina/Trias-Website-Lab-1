9465a1cc7132059be9f62a18d14ad4a3864bea02
Vielle <lstrias@up.edu.ph>
2022-09-15 16:45:39 +0800

    Website

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+    <title>LOGIN</title>
+    <link rel="stylesheet"type="text/css" href="indexes.css">
+</head>
+<body>
+    <?php
+    <form>
+            <h2>LOGIN</h2>
+            <label>Username</label>
+            <input type = "text" name="uname" placeholder="Username">
+            <label>Password</label>
+            <input type = "text" name="uname" placeholder="Password">
+            <button type="submit">Login</button>
+        </form>
+        ?>
+</body>
+</html>
